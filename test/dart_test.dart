7607ac7e5bb5e0eb6587c259b4aa96f10b1205c9
kyorohiro <kyorohiro@gmail.com>
2018-04-18 02:51:18 +0900

    fc

---
+++ test/dart_test.dart
@@ -0,0 +1,41 @@
+import 'package:markdown_with_metadata/markdown.dart' as markd;
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+
+    setUp(() {
+    });
+/*
+    test('not exist metadata', () async{
+      String convertedText = await markd.markdownToHtml("# h1");
+      print(convertedText);
+    });
+*/
+    test('meta data1', () async {
+      markd.MarkdownData markdownData = await markd.markdownToHtml(
+          "---\r\n" // 0-5
+          "a:b\r\n"  // 5-10
+          "test:test\r\n" // 11-22
+          "---\r\n"
+          "# test\r\n"
+          "Game programming\r\n");
+      expect(markdownData.metadata["a"], "b");
+      expect(markdownData.metadata["test"], "test");
+      print(markdownData.html);
+    });
+
+    test('meta data2', () async {
+      markd.MarkdownData markdownData = await markd.markdownToHtml(
+          "---\r\n" // 0-5
+              "a:b\r\n"  // 5-10
+              "test:test\r\n asdfasdf\r\n" // 11-22
+              "---\r\n"
+              "# test\r\n"
+              "Game programming\r\n");
+      expect(markdownData.metadata["a"], "b");
+      expect(markdownData.metadata["test"], "test\r\nasdfasdf");
+      print(markdownData.html);
+    });
+  });
+}
