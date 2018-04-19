d09e68cce08c22c5fbdf80c8e95dd7b8ec317eea
kyorohiro <kyorohiro@gmail.com>
2018-04-19 16:13:31 +0900

    v0.0.1

--- README.md
+++ README.md
@@ -19,11 +19,11 @@ import 'dart:async';
 Future<Null> main() async {
   HtmlAndMetadata data = await createHtml(
       "---\r\n"
-          "a:b\r\n"
-          "test:test\r\n"
-          "---\r\n"
-          "# test\r\n"
-          "Game programming\r\n");
+      "a:b\r\n"
+      "test:test\r\n"
+      "---\r\n"
+      "# test\r\n"
+      "Game programming\r\n");
   for(String k in data.metadata.keys) {
     print("${k} : ${data.metadata[k]}");
   }
--- example/main.dart
+++ example/main.dart
@@ -1,16 +1,18 @@
 import 'package:markdown_with_metadata/markdown.dart';
 import 'dart:async';
+import 'dart:convert' as conv;
 
 Future<Null> main() async {
   HtmlAndMetadata data = await createHtml(
       "---\r\n"
-          "a:b\r\n"
-          "test:test\r\n"
-          "---\r\n"
-          "# test\r\n"
-          "Game programming\r\n");
-  for(String k in data.metadata.keys) {
-    print("${k} : ${data.metadata[k]}");
-  }
+      "a:b\r\n"
+      "test:\r\n"
+      " test\r\n"
+      "---\r\n"
+      "# test\r\n"
+      "Game programming\r\n");
+
+  print(conv.JSON.encode(data.metadata));
   print(data.html);
 }
+
